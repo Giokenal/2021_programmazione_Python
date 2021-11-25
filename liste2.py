5014cdb67b08c6e3854ea016b3dd07e6306bfaf4
Giokenal <giovanni.mnz@gmail.com>
2021-11-25 16:57:51 +0100

    random

---
+++ liste2.py
@@ -0,0 +1,29 @@
+import random
+def random_select_nums_sample(n_list,n):
+    return random.sample(n_list,n)
+
+def random_select_nums_randint(n_list,n):
+    for i in range(0,n):
+        random_number= random.randint(0,len(n_list)-1)
+        print(random_number)
+        print(n_list[random_number])
+
+def random_select_nums_choices(n_list,n):
+
+        return random.choices(n_list,k=n)
+
+
+n_list=['a','a','b','f','h','k','t','t']
+selec_nums=3
+# new_list=[]
+# for element in n_list:
+#     if element not in new_list:
+#         new_list.append(element)
+result_sample=random_select_nums_sample(n_list,selec_nums)
+
+
+
+
+# random_select_nums_randint(n_list,selec_nums)
+# print (random_select_nums_choices(n_list,selec_nums))
+print (result_sample)
